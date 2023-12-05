60f4fd3507c086087d17d5b556c5eba08f8f8eca
Sangohan <89817683+iamSangohan@users.noreply.github.com>
2023-12-05 10:19:01 +0000

    #feat : liste produits

---
+++ AppGestionStock/ListeProduits.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace AppGestionStock
+{
+    partial class ListeProduits
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.quitter = new System.Windows.Forms.Button();
+            this.modifier = new System.Windows.Forms.Button();
+            this.supprimer = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajouterUnClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listeDesClientsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fournisseursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ajouterUnFournisseurToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produitsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facturesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.CBFournisseur = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(68, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(418, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Liste des produits";
+            // 
+            // quitter
+            // 
+            this.quitter.Location = new System.Drawing.Point(579, 401);
+            this.quitter.Name = "quitter";
+            this.quitter.Size = new System.Drawing.Size(153, 39);
+            this.quitter.TabIndex = 11;
+            this.quitter.Text = "Quitter";
+            this.quitter.UseVisualStyleBackColor = true;
+            this.quitter.Click += new System.EventHandler(this.quitter_Click);
+            // 
+            // modifier
+            // 
+            this.modifier.Location = new System.Drawing.Point(323, 401);
+            this.modifier.Name = "modifier";
+            this.modifier.Size = new System.Drawing.Size(153, 39);
+            this.modifier.TabIndex = 10;
+            this.modifier.Text = "Modifier";
+            this.modifier.UseVisualStyleBackColor = true;
+            this.modifier.Click += new System.EventHandler(this.modifier_Click);
+            // 
+            // supprimer
+            // 
+            this.supprimer.Location = new System.Drawing.Point(62, 401);
+            this.supprimer.Name = "supprimer";
+            this.supprimer.Size = new System.Drawing.Size(153, 39);
+            this.supprimer.TabIndex = 9;
+            this.supprimer.Text = "Supprimer";
+            this.supprimer.UseVisualStyleBackColor = true;
+            this.supprimer.Click += new System.EventHandler(this.supprimer_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(39, 164);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(718, 221);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(111, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(569, 83);
+            this.panel1.TabIndex = 7;
+            // 
+            // clientToolStripMenuItem
+            // 
+            this.clientToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajouterUnClientToolStripMenuItem,
+            this.listeDesClientsToolStripMenuItem});
+            this.clientToolStripMenuItem.Name = "clientToolStripMenuItem";
+            this.clientToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.clientToolStripMenuItem.Text = "Client";
+            // 
+            // ajouterUnClientToolStripMenuItem
+            // 
+            this.ajouterUnClientToolStripMenuItem.Name = "ajouterUnClientToolStripMenuItem";
+            this.ajouterUnClientToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.ajouterUnClientToolStripMenuItem.Text = "Ajouter un client";
+            // 
+            // listeDesClientsToolStripMenuItem
+            // 
+            this.listeDesClientsToolStripMenuItem.Name = "listeDesClientsToolStripMenuItem";
+            this.listeDesClientsToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.listeDesClientsToolStripMenuItem.Text = "Liste des clients";
+            // 
+            // fournisseursToolStripMenuItem
+            // 
+            this.fournisseursToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ajouterUnFournisseurToolStripMenuItem});
+            this.fournisseursToolStripMenuItem.Name = "fournisseursToolStripMenuItem";
+            this.fournisseursToolStripMenuItem.Size = new System.Drawing.Size(85, 20);
+            this.fournisseursToolStripMenuItem.Text = "Fournisseurs";
+            // 
+            // ajouterUnFournisseurToolStripMenuItem
+            // 
+            this.ajouterUnFournisseurToolStripMenuItem.Name = "ajouterUnFournisseurToolStripMenuItem";
+            this.ajouterUnFournisseurToolStripMenuItem.Size = new System.Drawing.Size(192, 22);
+            this.ajouterUnFournisseurToolStripMenuItem.Text = "Ajouter un fournisseur";
+            // 
+            // produitsToolStripMenuItem
+            // 
+            this.produitsToolStripMenuItem.Name = "produitsToolStripMenuItem";
+            this.produitsToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.produitsToolStripMenuItem.Text = "Produits";
+            // 
+            // facturesToolStripMenuItem
+            // 
+            this.facturesToolStripMenuItem.Name = "facturesToolStripMenuItem";
+            this.facturesToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.facturesToolStripMenuItem.Text = "Factures";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
+            this.toolStripMenuItem1.Text = "?";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientToolStripMenuItem,
+            this.fournisseursToolStripMenuItem,
+            this.produitsToolStripMenuItem,
+            this.facturesToolStripMenuItem,
+            this.toolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // CBFournisseur
+            // 
+            this.CBFournisseur.FormattingEnabled = true;
+            this.CBFournisseur.Location = new System.Drawing.Point(369, 137);
+            this.CBFournisseur.Name = "CBFournisseur";
+            this.CBFournisseur.Size = new System.Drawing.Size(159, 21);
+            this.CBFournisseur.TabIndex = 20;
+            this.CBFournisseur.SelectionChangeCommitted += new System.EventHandler(this.CBFournisseur_SelectionChangeCommitted);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(281, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Fournisseur";
+            // 
+            // ListeProduits
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CBFournisseur);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.quitter);
+            this.Controls.Add(this.modifier);
+            this.Controls.Add(this.supprimer);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "ListeProduits";
+            this.Text = "ListeProduits";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button quitter;
+        private System.Windows.Forms.Button modifier;
+        private System.Windows.Forms.Button supprimer;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStripMenuItem clientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajouterUnClientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listeDesClientsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fournisseursToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ajouterUnFournisseurToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produitsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facturesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ComboBox CBFournisseur;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ AppGestionStock/ListeProduits.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppGestionStock
+{
+    public partial class ListeProduits : Form
+    {
+        SqlConnection cnx;
+        SqlCommand cmd;
+        SqlDataAdapter adap;
+        SqlCommandBuilder cb;
+        DataSet dset, dset2;
+
+        public ListeProduits()
+        {
+            InitializeComponent();
+
+            cnx = new SqlConnection();
+            cnx.ConnectionString = "Data Source=DESKTOP-OQ50T8E;Initial Catalog=BDStock;Integrated Security=True;Encrypt=False;TrustServerCertificate=True";
+            cnx.Open();
+            cmd = new SqlCommand();
+            cmd.CommandText = "select * from Fournisseur";
+            cmd.Connection = cnx;
+            adap = new SqlDataAdapter(cmd);
+            dset = new DataSet();
+            adap.Fill(dset, "Fournisseur");
+            DataRow dr;
+            dr = dset.Tables[0].NewRow();
+            dr["NomPrenom"] = "Toute la liste";
+            dset.Tables[0].Rows.Add(dr);
+            CBFournisseur.DataSource = dset.Tables[0];
+            CBFournisseur.DisplayMember = "NomPrenom";
+            CBFournisseur.ValueMember = "CodeFourn";
+            cmd = new SqlCommand();
+            cmd.CommandText = "select CodeProduit,NomProduit,NomPrenom as 'Fournisseur', PrixUnitaire, QtEnStock from Produit P, Fournisseur F where P.CodeFourn = F.CodeFourn";
+            cmd.Connection = cnx;
+            adap = new SqlDataAdapter(cmd);
+            dset2 = new DataSet();
+            adap.Fill(dset2, "Produit");
+            dataGridView1.DataSource = dset2.Tables[0];
+            CBFournisseur.SelectedIndex = CBFournisseur.Items.Count - 1;
+        }
+
+        private void supprimer_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Voulez vous vraiment supprimer ce produit ? ", "Supprimer un Produit", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    cmd.CommandText = "delete from Produit where CodeProduit=" +
+
+                    dataGridView1.CurrentRow.Cells[0].Value.ToString();
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("suppression effectuée avec succes", "Suppression");
+                    cmd = new SqlCommand();
+                    cmd.CommandText = "select CodeProduit,NomProduit,NomPrenom as 'Fournisseur', PrixUnitaire, QtEnStock from Produit P, Fournisseur F where P.CodeFourn = F.CodeFourn";
+                    cmd.Connection = cnx;
+                    adap = new SqlDataAdapter(cmd);
+                    dset2 = new DataSet();
+                    adap.Fill(dset2, "Produit");
+                    dataGridView1.DataSource = dset2.Tables[0];
+                    CBFournisseur.SelectedIndex = CBFournisseur.Items.Count - 1;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void modifier_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (MessageBox.Show("Voulez vous enregistrer les modifications ? ", "Confirmer modification", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    cb = new SqlCommandBuilder(adap);
+                    adap.Update(dset, "Produit");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void quitter_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CBFournisseur_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            cmd = new SqlCommand();
+            if (CBFournisseur.SelectedIndex == CBFournisseur.Items.Count - 1) {
+                cmd.CommandText = "select CodeProduit,NomProduit,NomPrenom as 'Fournisseur', PrixUnitaire, QtEnStock from Produit P, Fournisseur F where P.CodeFourn = F.CodeFourn";
+
+            }
+            else {
+                cmd.CommandText = "select CodeProduit,NomProduit,NomPrenom as 'Fournisseur', PrixUnitaire, QtEnStock from Produit P, Fournisseur F where P.CodeFourn = F.CodeFourn and P.CodeFourn = " + CBFournisseur.SelectedValue;
+                cmd.Connection = cnx;
+                adap = new SqlDataAdapter(cmd);
+                dset2 = new DataSet();
+                adap.Fill(dset2, "Produit");
+                dataGridView1.DataSource = dset2.Tables[0];
+            }
+        }
+    }
+}
